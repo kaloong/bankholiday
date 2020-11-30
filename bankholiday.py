3f41c03382445f21a4a0bbd6bbd49513619962dc
Kaloong <kaloong@gmail.com>
2020-11-30 23:12:36 +0000

    Modified fetch method with exception cases.

--- bankholiday.py
+++ bankholiday.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime, sys
 import icalendar
 import requests
 
@@ -6,12 +6,22 @@ import requests
 today=datetime.date(2017,4,17)
 def fetch_latest():
 	url="https://www.gov.uk/bank-holidays/england-and-wales.ics"
-	req= requests.get(url, allow_redirects=True)
+	try:
+		req= requests.get(url, allow_redirects=True)
 
-	if req.status_code == 200:
-		with open('bankholiday.ics','wb') as f:
-			f.write(req.content)
-	return req.content
+		if req.status_code == 200:
+			with open('bankholiday.ics','wb') as f:
+				f.write(req.content)
+			return req.content
+	except requests.exceptions.ConnectionError as e:
+		print("Error: Connection Error.\n",e)
+	except requests.exceptions.HTTPError as e:
+		print("Error: HTTP Error:\n",e)
+	except requests.exceptions.RequestException as e:
+		print("Error: Something is wrong.\n", e)
+	except requests.exceptions.Timeout as e:
+		print("Error: Connection timed out.\n",e)
+	sys.exit()
 
 data=fetch_latest()
 ical=icalendar.Calendar.from_ical(data)
